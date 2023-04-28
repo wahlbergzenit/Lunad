23b9fc51e07764dfb3023dc2c6823be79576f1cf
Wahlberg Tristan Zenit <wzenit@gbox.adnu.edu.ph>
2023-04-28 15:52:00 +0800

    Collapsing Sidebar now working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="script.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
     <script src="./Bootstrap/js/bootstrap.bundle.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
