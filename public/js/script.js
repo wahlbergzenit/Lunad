3c0616adcbbc5c52740f8ffee35780b2a33fae5e
Wahlberg Tristan Zenit <wzenit@gbox.adnu.edu.ph>
2023-05-06 19:10:34 +0800

    finally added a map

--- public/js/script.js
+++ public/js/script.js
@@ -35,4 +35,20 @@ this.classList.add('active')
 linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
     // Your code to run since DOM is loaded and ready
+    const map = L.map('map', {
+        center: [-29.50, 145],
+        zoom: 3.5
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
+    const marker1 = L.marker([-37.699450, 176.279420]).addTo(map);
+    const marker2 = L.marker([-27.643310, 153.305140]).addTo(map);
+    const marker3 = L.marker([-33.956330, 122.150270]).addTo(map);
+    const marker4 = L.marker([-34.962390, 117.391220]).addTo(map);
+    const marker5 = L.marker([-17.961210, 122.214820]).addTo(map);
+    const marker6 = L.marker([-16.505960, 151.751520]).addTo(map);
+    const marker7 = L.marker([-22.594400, 167.484440]).addTo(map);
+    const marker8 = L.marker([-37.977000, 177.057000]).addTo(map);
+    const marker9 = L.marker([-41.037600, 173.017000]).addTo(map);
+    const marker10 = L.marker([-37.670300, 176.212000]).addTo(map);
 });
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,6 @@
 
     <title>Lunad Maps</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/script.js') }}"></script>
-    <script src="./Bootstrap/js/bootstrap.bundle.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -29,90 +22,105 @@
     <link rel="stylesheet" href="./Bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+        crossorigin=""/>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="./Bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     
 </head>
 <body id="body-pd" oncontextmenu="return false" class="snippet-body body-pd">
-    <div id="app">
+    <div id="map"></div>
     <!-- Navbar -->
-    <header class="header navbar-expand-md body-pd" id="header">
-        <div class="navbar-nav gap-2 align-middle">
-            <div class="header_toggle me-2 mt-2">
-                <i class='bx bx-arrow-back' id="header-toggle"></i>
+    <div class="lunad_UI">
+        <header class="header navbar-expand-md body-pd" id="header">
+            <div class="navbar-nav gap-2 align-middle">
+                <div class="header_toggle me-2 mt-2">
+                    <i class='bx bx-arrow-back' id="header-toggle"></i>
+                </div>
+                <button type="button" href="/ride/0" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('tricycle') ? 'active' : ''}}">
+                    <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/tricy.png')}}" width="20"></img>
+                    Tricycle</button>
+                <button type="button" href="/ride/1" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('jeepney') ? 'active' : ''}}">
+                    <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/jeepney.png')}}" width="20"></img>
+                    Jeepney</button>
+                <button type="button" href="/ride/2" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('pedicab') ? 'active' : ''}}">
+                    <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/pedicab.png')}}" width="20"></img>
+                    Pedicab</button>
+                <button type="button" href="/ride/3" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('bus') ? 'active' : ''}}">
+                    <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/bus.png')}}" width="20"></img>
+                    Bus</button>
+                <button type="button" href="/ride/4" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('van') ? 'active' : ''}}">
+                    <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/van.png')}}" width="20"></img>
+                    Van</button>
             </div>
-            <button type="button" href="/ride/0" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('tricycle') ? 'active' : ''}}">
-                <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/tricy.png')}}" width="20"></img>
-                Tricycle</button>
-            <button type="button" href="/ride/1" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('jeepney') ? 'active' : ''}}">
-                <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/jeepney.png')}}" width="20"></img>
-                Jeepney</button>
-            <button type="button" href="/ride/2" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('pedicab') ? 'active' : ''}}">
-                <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/pedicab.png')}}" width="20"></img>
-                Pedicab</button>
-            <button type="button" href="/ride/3" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('bus') ? 'active' : ''}}">
-                <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/bus.png')}}" width="20"></img>
-                Bus</button>
-            <button type="button" href="/ride/4" class="btn btn-primary h-25 fw-bold pe-3 mt-2 rounded-pill shadow map-select {{request()->is('van') ? 'active' : ''}}">
-                <img class="v-icon mx-1" src="{{asset('img/Icons/Vehicle Icons/van.png')}}" width="20"></img>
-                Van</button>
-        </div>
-    </header>
-    <!-- Sidebar -->
-    <div class="l-navbar show shadow" id="nav-bar">
-        <nav class="nav">
-            <div class="saved_routes pt-4 pb-3 shadow-sm">
-                <i class='bx bx-bookmark nav_icon'></i>
-                <span class="nav_name ms-2 fw-bold">Saved Routes</span>
-            </div>
+        </header>
+        <!-- Sidebar -->
+        <div class="l-navbar show shadow" id="nav-bar">
+            <nav class="nav">
+                <div class="saved_routes pt-4 pb-3 shadow-sm">
+                    <i class='bx bx-bookmark nav_icon'></i>
+                    <span class="nav_name ms-2 fw-bold">Saved Routes</span>
+                </div>
 
-            <!-- Saved routes list -->
-            <div class="nav_list ms-5 mb-auto">
-                <a href="#" class="route_link">
-                <i class='bx bx-pin nav_icon'></i> <span class="nav_name">route 1</span> </a>
-            </div>
-            
-            <div class="navbar navbar-expand-md shadow ps-4 py-2 user_bar">
-                <i class='bx bx-user user_icon'></i>
-                <div class="gap-1 user_text">
-                <ul class="navbar-nav">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            <div class="vr my-auto" style="width: 2px; height: 20px;"></div>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                <!-- Saved routes list -->
+                <div class="nav_list ms-5 mb-auto">
+                    <a href="#" class="route_link">
+                    <i class='bx bx-pin nav_icon'></i> <span class="nav_name">route 1</span> </a>
+                </div>
+                
+                <div class="navbar navbar-expand-md shadow ps-4 py-2 user_bar">
+                    <i class='bx bx-user user_icon'></i>
+                    <div class="gap-1 user_text">
+                        <ul class="navbar-nav">
+                            <!-- Authentication Links -->
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
+                                <div class="vr my-auto" style="width: 2px; height: 20px;"></div>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     </div>
-    <!--Container Main start-->
     
+    <!--Container Main start-->
     <!--Container Main end-->
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+        crossorigin=""></script>
 </body>
 </html>
